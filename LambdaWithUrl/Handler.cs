fad65082e685b9facef8c694f9439eb25ba1d635
kurt <feeleyfire@gmail.com>
2022-05-04 19:27:12 -0400

    initial commit

---
+++ LambdaWithUrl/Handler.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Serialization.Json;
+
+namespace LambdaWithUrl;
+
+public class Handler
+{
+    [LambdaSerializer(typeof(JsonSerializer))]
+    public IEnumerable<String> Handle(APIGatewayProxyRequest apiGatewayProxyRequest)
+    {
+        return apiGatewayProxyRequest?.QueryStringParameters?.Values ?? new String[0];
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# lambda-with-url
+# AWS Lambda URLs
+
+This repo contains example code for the blog post at:
+
+https://awswith.net/2022/05/01/create-a-net-aws-lambda-function-with-an-https-endpoint/
